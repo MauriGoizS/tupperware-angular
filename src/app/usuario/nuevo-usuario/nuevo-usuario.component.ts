75d1a476be10f601c88568a46a8f714d59a1993b
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-04-09 18:36:23 -0600

    Se implemento para el nuevo usuario

--- src/app/usuario/nuevo-usuario/nuevo-usuario.component.ts
+++ src/app/usuario/nuevo-usuario/nuevo-usuario.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { NewUsuarioService } from './../../service/new-usuario.service';
 import { catusuario } from 'src/app/modelo/catusuario';
-
 
 @Component({
   selector: 'app-nuevo-usuario',
@@ -10,4 +10,39 @@ import { catusuario } from 'src/app/modelo/catusuario';
 })
 export class NuevoUsuarioComponent {
 
+  image: string = '';
+  misUsuarios: catusuario = {
+    _id:'',
+    foto: '',
+    gmail: '',
+    rol: '',
+    password: ''
+  };
+
+  constructor (
+    private router: Router,
+    private newUsuarioService: NewUsuarioService
+  ) {}
+
+  convertir_B64(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.image = reader.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  enviarUsuario() {
+    this.newUsuarioService.new_usuario(this.misUsuarios).subscribe(data => {
+      console.log("Usuario", data);
+      if (data) {
+        this.router.navigateByUrl('/catalogo-usuario');
+      } else {
+        console.log("error");
+      }
+    });
+  }
 }
