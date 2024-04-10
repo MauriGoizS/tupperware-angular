2ae657deba877af292fcc37db80da3a5241aee91
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-04-10 01:17:46 -0600

    Se agrego la funcionalidad de eliminar cliente

--- src/app/cliente/eliminar-cliente/eliminar-cliente.component.ts
+++ src/app/cliente/eliminar-cliente/eliminar-cliente.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { CatClienteService } from 'src/app/service/cliente/cat-cliente.service';
 
 @Component({
   selector: 'app-eliminar-cliente',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./eliminar-cliente.component.css']
 })
 export class EliminarClienteComponent {
+  clienteId:string | null= null;
+  constructor(private ruta: Router,
+    private router: ActivatedRoute,
+    private servidorCliente: CatClienteService
+    ){}
 
+ngOnInit(): void {
+  this.clienteId = this.router.snapshot.paramMap.get('id');
+  console.log(this.clienteId);
+  this.eliminar()
+  }
+
+res:any
+messageErr:any
+eliminar(){
+try {
+this.res=this.servidorCliente.eliminar_cliente(this.clienteId)
+.subscribe(data=>{
+console.log(data)})
+alert("Cliente eliminado")
+//redirecciona para cargar el componente catálogo
+this.ruta.navigate(['/catalogo-cliente'])
+}
+catch(error: any) {
+this.messageErr = error.error.message
+}
+}
 }
