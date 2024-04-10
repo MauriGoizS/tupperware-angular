56ad6a54146c6c92bad04cbed686f0149c81be02
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-04-09 22:54:02 -0600

    Creando los componentes de cliente

---
+++ src/app/cliente/catalogo-cliente/catalogo-cliente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo-cliente',
+  templateUrl: './catalogo-cliente.component.html',
+  styleUrls: ['./catalogo-cliente.component.css']
+})
+export class CatalogoClienteComponent {
+
+}
--- src/app/cliente/cliente.module.ts
+++ src/app/cliente/cliente.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActualizarClienteComponent } from './actualizar-cliente/actualizar-cliente.component';
+import { CatalogoClienteComponent } from './catalogo-cliente/catalogo-cliente.component';
+import { EliminarClienteComponent } from './eliminar-cliente/eliminar-cliente.component';
+import { NuevoClienteComponent } from './nuevo-cliente/nuevo-cliente.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ActualizarClienteComponent,
+    CatalogoClienteComponent,
+    EliminarClienteComponent,
+    NuevoClienteComponent
+  ],
   imports: [
     CommonModule
   ]
