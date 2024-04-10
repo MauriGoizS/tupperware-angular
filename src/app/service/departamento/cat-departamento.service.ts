b364fd17b117a235b8d5ba664e1d1e3d68c23fb3
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-04-10 01:37:02 -0600

    Agregando los endpoint a los servicios de catalogo

--- src/app/service/departamento/cat-departamento.service.ts
+++ src/app/service/departamento/cat-departamento.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
-
+import { catdepartamento } from 'src/app/modelo/catdepartamento';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class CatDepartamentoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  obtenercat_departamento(){
+    return this.http.get<catdepartamento[]>('http://127.0.0.1:4000/departamento/get_all')
+
+  }
+
+  eliminar_departamento(id:string | null){
+    const apiUrl='http://127.0.0.1:4000'
+    const url = `${apiUrl}/departamento/eliminar/${id}`;
+    return this.http.delete<any>(url)
+    }
+
+    obtenerPorId(id: string): Observable<catdepartamento> {
+      const apiUrl = 'http://127.0.0.1:4000';
+      const url = `${apiUrl}/departamento/porID/${id}`;
+      return this.http.get<catdepartamento>(url);
+    }
+
+  actualizardepartamento(id: string, producto: Partial<catdepartamento>): Observable<any> {
+    const apiUrl = 'http://127.0.0.1:4000';
+    const url = `${apiUrl}/departamento/actualizar/${id}`;
+    const headers = { 'Content-Type': 'application/json' };
+
+    return this.http.put<any>(url, producto, { headers })
+      .pipe(tap((res: any) => {
+          console.log("Servicio de actualización:", res.message);
+        }),
+        catchError(err => of(err.error.message))
+      );
+  }
+
 }
+
--- src/app/service/departamento/new-departamento.service.ts
+++ src/app/service/departamento/new-departamento.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { catdepartamento } from 'src/app/modelo/catdepartamento';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewDepartamentoService {
 
-  constructor() { }
+  private apiUrl = 'http://127.0.0.1:4000/departamento/nuevodepartamento';
+
+  constructor(private http:HttpClient) { }
+
+  new_departamento(miDepartamento:catdepartamento):Observable<any>{
+    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.post<any>(this.apiUrl, miDepartamento,{headers}).pipe(
+      tap((res: any)=>{
+        if (res.message == "departamento insertado"){
+          console.log("servicio", res.message);
+        }
+      }),
+      catchError(err=> of (err.error.message))
+    );
+  }
 }
