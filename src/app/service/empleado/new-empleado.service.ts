7ec90315d71b69eb4fdec55eb5a5ecf3cc872fb0
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-04-09 22:05:30 -0600

    Haciendo lo de insertar un nuevo empleado

--- src/app/service/empleado/new-empleado.service.ts
+++ src/app/service/empleado/new-empleado.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { catempleado } from 'src/app/modelo/catempleado';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewEmpleadoService {
 
-  constructor() { }
+  private apiUrl = 'http://127.0.0.1:4000/empleado/nuevoempleado';
+
+  constructor(private http:HttpClient) { }
+
+  new_empleado(miEmpleado:catempleado):Observable<any>{
+    const headers = new HttpHeaders({'Content-Type': 'application/json'});
+    return this.http.post<any>(this.apiUrl, miEmpleado,{headers}).pipe(
+      tap((res: any)=>{
+        if (res.message == "empleado insertado"){
+          console.log("servicio", res.message);
+        }
+      }),
+      catchError(err=> of (err.error.message))
+    );
+  }
 }
