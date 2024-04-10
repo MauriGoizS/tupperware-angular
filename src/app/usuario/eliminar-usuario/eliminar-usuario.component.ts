16006ad8141646bbc205290914610dadb575d389
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-04-09 19:43:38 -0600

    Se agrego la funcionalidad de eliminar usuario

--- src/app/usuario/eliminar-usuario/eliminar-usuario.component.ts
+++ src/app/usuario/eliminar-usuario/eliminar-usuario.component.ts
@@ -1,4 +1,7 @@
+import { CatUsuarioService } from 'src/app/service/cat-usuario.service';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-eliminar-usuario',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./eliminar-usuario.component.css']
 })
 export class EliminarUsuarioComponent {
+  usuarioId:string | null= null;
+  constructor(private ruta: Router,
+    private router: ActivatedRoute,
+    private servidorUsuario: CatUsuarioService
+    ){}
 
+ngOnInit(): void {
+  this.usuarioId = this.router.snapshot.paramMap.get('id');
+  console.log(this.usuarioId);
+  this.eliminar()
+  }
+
+res:any
+messageErr:any
+eliminar(){
+try {
+this.res=this.servidorUsuario.eliminar_usuario(this.usuarioId)
+.subscribe(data=>{
+console.log(data)})
+alert("usuario eliminado")
+//redirecciona para cargar el componente catálogo
+this.ruta.navigate(['/catalogo-usuario'])
+}
+catch(error: any) {
+this.messageErr = error.error.message
+}
+}
 }
