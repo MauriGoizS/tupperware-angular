ababea16fcd155cdcb0de1783a9071c3b780dc89
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-04-10 00:16:01 -0600

    Agregando funcionalidad de nuevo cliente

--- src/app/cliente/nuevo-cliente/nuevo-cliente.component.ts
+++ src/app/cliente/nuevo-cliente/nuevo-cliente.component.ts
@@ -1,10 +1,40 @@
+import { NewClienteService } from './../../service/cliente/new-cliente.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { catcliente } from 'src/app/modelo/catcliente';
 
 @Component({
   selector: 'app-nuevo-cliente',
   templateUrl: './nuevo-cliente.component.html',
   styleUrls: ['./nuevo-cliente.component.css']
 })
-export class NuevoClienteComponent {
 
+export class NuevoClienteComponent {
+  miCliente: catcliente = {
+    _id: '',
+    correo: '',
+    password: '',
+    tel: '',
+    edad: 0,
+    direccion: '',
+    nomuser: ''
+  };
+
+  constructor (
+    private router: Router,
+    private NewClienteService: NewClienteService
+  ) {}
+
+
+
+  enviarCliente() {
+    this.NewClienteService.new_cliente(this.miCliente).subscribe(data => {
+      console.log("Cliente", data);
+      if (data) {
+        this.router.navigateByUrl('/catalogo-cliente');
+      } else {
+        console.log("error");
+      }
+    });
+  }
 }
